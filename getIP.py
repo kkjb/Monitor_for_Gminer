e9ce4ad0c66c81bd46e11ec7f588f31cba85fb73
kkjb <9390833+kkjb@users.noreply.github.com>
2021-09-15 16:52:46 +0800

    added file save
    add file save function to save active hosts IP to the files

--- getIP.py
+++ getIP.py
@@ -33,6 +33,7 @@ def is_port_open(ip, port):
 def open_multithreaded_detect_ip(ip,port):
     threads = []
     for i in range(0,256):
+        # Regular Expression for testing IP from 0 to 255
         ip_tested =  re.sub("\d{1,3}$",str(i),ip)
         t = threading.Thread(target=is_port_open,args=(ip_tested,port))
         t.start()
@@ -41,15 +42,35 @@ def open_multithreaded_detect_ip(ip,port):
     for t in threads:
         t.join()
     
+def save_active_hosts_to_file(active_hosts):
+    filename = "Active_miner_IP.txt"
 
+    if active_hosts:
+        # open and clean old content, if not exists a file will be created
+        fw = open(filename, 'w')
+        for element in active_hosts:    
+            fw.write(element + "\n")
+        fw.close()
+    else:
+        print("There is no active miner in this LAN：" + str(re.sub("\d{1,3}$","X",ip)))
 
 if __name__ == '__main__':
     starttime = datetime.datetime.now()
-
-    ip = get_host_ip()
+    # port must be set
     port = 8848
+
+    # a global list for the multi-thread scannings of the active hosts
     active_hosts = []
+
+    # get localhost IP in the LAN
+    ip = get_host_ip()
+
+    # multi-thread scan the open port in the LAN
     open_multithreaded_detect_ip(ip,port)
+    
+    # save detected active_hosts to file
+    save_active_hosts_to_file(active_hosts)
+
     print(active_hosts)
     
     
